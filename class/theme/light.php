bc2b36308bdc41282c83d864f7b589c4469f4a64
Miguel Escamilla <metuxxman@gmail.com>
2015-01-15 21:19:30 -0800

    add commet for TCard propertys and objectCards as a test client

---
+++ class/theme/light.php
@@ -0,0 +1,30 @@
+<?
+//Class creators :Custome card properties
+include('Int/ICard');
+class light extends TCard{
+
+//  public $description;
+//  public $name;
+//  public $date;
+//  public $title;
+//  public $category;
+
+	protected public $likeValue;
+public function __construct(TCard $oldcard,$lik,){
+
+$this->likeValue=$lik;	//0-5 value:
+
+
+}
+
+}
+
+//Controller
+
+class controller implements View{
+
+	public function display(){
+
+	}
+}
+?>
--- objectCards.php
+++ objectCards.php
@@ -1,8 +1,18 @@
 <?
-//Client:
-
-class heartcard extends TCard{
-	
+foreach (glob("Int/*.php") as $filename)
+{
+    include $filename;
 }
 
+
+//$t,$d,$n,$c,$da
+$ccard= new TCard(
+"sampleTitle",
+,"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta porro repellat voluptas iure facilis vero minus deserunt sint aspernatur. 
+Labore ullam dolorum temporibus. Expedita delectus iusto totam nostrum temporibus voluptatum?",
+"testing","2015-Jan-16"
+
+	);
+$them= new myt();
+$vcard= new View($ccard,$them);
 ?>
